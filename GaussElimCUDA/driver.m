43a76b5033796e272bd4e138f5838d43326e2939
dmitchell412 <drew.mitchell412@gmail.com>
2014-03-26 10:34:41 -0500

    working version

--- GaussElimCUDA/driver.m
+++ GaussElimCUDA/driver.m
@@ -6,16 +6,18 @@ format shortg
 % create example matrix solve
 Nsize = 10;
 h_A = rand(Nsize);
+%h_A = [6,-1,-2;-6,13,-6;-2,-1,6];
 h_b = rand(Nsize,1);
-h_C = [h_A,h_b];
+%h_b = [3;1;3];
+%h_C = [h_A,h_b];
+%h_C = [6,-1,-2,3;-6,13,-6,1;-2,-1,6,3]
 h_x = h_A\h_b
 
 % transfer data to device
 %d_A  = gpuArray( h_A );
 %d_b  = gpuArray( h_b );
-d_C  = gpuArray( h_C );
-d_x  = gpuArray( zeros(Nsize,1) );
-
+d_C  = gpuArray( [h_A,h_b] );
+d_Pivot  = gpuArray( zeros(Nsize,Nsize+1) );
 
 GaussSolveptx = parallel.gpu.CUDAKernel('GaussSolve.ptx', 'GaussSolve.cu');
 threadsPerBlock = 256;
@@ -24,10 +26,20 @@ GaussSolveptx.ThreadBlockSize=[threadsPerBlock  1];
 blocksPerGrid = (npixel  + threadsPerBlock - 1) / threadsPerBlock;
 GaussSolveptx.GridSize=[ceil(blocksPerGrid)  1];
 
-[d_x ] = feval(GaussSolveptx,Nsize,d_C,d_x);
-
+[dAout,dPout] = feval(GaussSolveptx,Nsize,d_C,d_Pivot);
+h_Pivot = gather(d_Pivot);
 h_C = gather(d_C);
-h_C
-%mysoln = gather(d_x);
+%dAout
 
-%norm(mysoln-h_x)
+% Backward substition
+mysoln = dAout(:,Nsize+1);
+for i=Nsize:-1:1
+    for j=Nsize:-1:i+1
+        mysoln(i) = mysoln(i) - dAout(i,j)*mysoln(j);
+    end
+    mysoln(i) = mysoln(i)/dAout(i,i);
+end
+mysoln
+%mysoln = gather(d_Pivot);
+
+norm(mysoln-h_x)
